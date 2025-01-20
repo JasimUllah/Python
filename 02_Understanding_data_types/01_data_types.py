f691627eefeff1d47c530bba551dcf154b206a9a
JasimUllah <jasimullah3@gmail.com>
2025-01-20 20:44:54 +0500

    Adding new folder

---
+++ 02_Understanding_data_types/01_data_types.py
@@ -0,0 +1,22 @@
+# len() function cannot be used directly with numbers
+# i.e. len(123432)
+
+# Data Types
+# 1. Strings
+# 2. int
+# 3. float
+
+# We can print a character using index
+# To pull out a particular number from a string is known as subscripting
+print("Hello"[0])
+
+
+# Integer declaration
+print(123 + 345)
+
+# Float 
+3.14159
+
+# Boolean
+True
+False
