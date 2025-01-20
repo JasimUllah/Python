2fe102a9f0a3e96aa9de80e3e881e2010c36289f
JasimUllah <jasimullah3@gmail.com>
2025-01-20 20:48:03 +0500

    Type checking

---
+++ 02_Understanding_data_types/02_type_checking_&_conversion.py
@@ -0,0 +1,22 @@
+# len() function does not take an integer
+
+# x = len(input("What is your name? "))
+# print("Your name has " + x + "characters")
+
+# After runnin this we will get an error for cancatenation
+# string cannot be concatenated with an integer
+
+# how to see the data type
+# print(type(x))
+
+
+#  Doing typcasting or type conversion
+# new_x = str(x)
+# print("Your name has " + new_x + " characters")
+
+
+# Converting an integer to a string
+a = str(123)
+print(type(a))
+
+print(str(100) + str(28))
