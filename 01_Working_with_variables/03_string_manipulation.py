549757dad3ff5979282dc869b5d1071cd010fb3b
JasimUllah <jasimullah3@gmail.com>
2025-01-20 20:40:03 +0500

    Third commit

---
+++ 01_Working_with_variables/03_string_manipulation.py
@@ -0,0 +1,8 @@
+# print("Hello World! \nHello World!")
+
+# We can concatenate strings by using a plus (+) sign
+print("Hello " + "Jasim")
+# OR
+print("Hello" + " Jasim")
+# OR
+print("Hello" + " " + "Jasim")
